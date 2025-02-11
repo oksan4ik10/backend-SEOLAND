388ba427f25c04aabfae838283948b46dda1ea44
Kalinina Oksana <ksan4ik92@gmail.com>
2025-02-11 15:02:56 +0300

    feat: added login endpoint

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    ATLAS_URI: 'mongodb+srv://admin:1234@cluster0.9dw2jf9.mongodb.net/staleved',
-    keys: 'dev-jwt',
-    keysReset: 'res-jwt'
-}
+  ATLAS_URI: "mongodb+srv://admin:1234@cluster0.9dw2jf9.mongodb.net/seoland",
+  keys: "dev-jwt",
+  keysReset: "res-jwt",
+};
